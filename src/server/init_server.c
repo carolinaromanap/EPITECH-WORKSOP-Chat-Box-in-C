aff072b42cb2437c57fac08a88ad3b946fc83f8c
Carolina Romana Pires <carolina.romana-pires@epitech.eu>
2022-06-10 10:54:03 +0200

    [ADD]  modif init server

--- src/server/init_server.c
+++ src/server/init_server.c
@@ -13,11 +13,16 @@ server_t *init_server(char *ip_addr, int port)
 
     server->ip_addr = ip_addr;
     server->port = port;
-
-    // init socket
-    // init server addr
-    // bind
-    // listen
-
+    server->socket =  socket(AF_INET, SOCK_STREAM, 6);
+    server->addr.sin_family = AF_INET;
+    server->addr.sin_addr.s_addr = inet_addr(server->ip_addr);
+    if (bind(server->socket,(struct sockaddr*)&server->addr,sizeof(server->addr)) == -1) {
+        perror("Bind");
+        return NULL;
+    }
+    if (listen(server->socket, 50) == -1) {
+        perror("Listen");
+        return NULL;
+    }
     return server;
 }
